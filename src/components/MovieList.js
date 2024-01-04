e354688a24a4b504ea46f98663fe31594004a2ef
Haseeb-creator <mdhaseeb769@gmail.com>
2024-01-04 15:53:52 +0530

    creating movie list to list down all movies

---
+++ src/components/MovieList.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import MovieCard from './MovieCard'
+
+const MovieList = ({ title, movies }) => {
+	return (
+		<div className="px-4">
+			<h1 className='text-2xl py-2 text-white'>{title}</h1>
+			<div className='flex overflow-x-scroll '>
+				<div className='flex'>
+					{movies?.map((movie) =>
+						<MovieCard key={movie.id} posterPath={movie.poster_path} />
+					)}
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default MovieList
