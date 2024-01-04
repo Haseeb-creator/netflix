124873d9e1c52e84778502c07daa249ca3f5f4a6
Haseeb-creator <mdhaseeb769@gmail.com>
2024-01-04 22:09:24 +0530

    adding multi lingual

---
+++ src/utils/languageConstants.js
@@ -0,0 +1,20 @@
+const lng = {
+	english: {
+		search: 'search',
+		gptSearchPlaceHolder: 'What would you like to Watch today?'
+	},
+	hindi: {
+		search: 'खोज',
+		gptSearchPlaceHolder: 'आज आप क्या देखना चाहेंगे?'
+	},
+	kannada: {
+		search: 'ಹುಡುಕಿ',
+		gptSearchPlaceHolder: 'ನೀವು ಇಂದು ಏನನ್ನು ವೀಕ್ಷಿಸಲು ಬಯಸುತ್ತೀರಿ?'
+	},
+	spanish: {
+		search: 'buscar',
+		gptSearchPlaceHolder: '¿Qué te gustaría ver hoy?'
+	}
+}
+
+export default lng
