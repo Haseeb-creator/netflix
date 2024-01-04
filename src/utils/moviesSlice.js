785e2c4440b0aa8932aa78fc0c34ea266d43db89
Haseeb-creator <mdhaseeb769@gmail.com>
2024-01-04 15:56:48 +0530

    added different slice to store data in store

--- src/utils/moviesSlice.js
+++ src/utils/moviesSlice.js
@@ -4,11 +4,23 @@ const movieSlice = createSlice({
 	name: "movie",
 	initialState: {
 		nowPlayingMovies: null,
+		popularMovies: null,
+		topRatedMovies: null,
+		upComingMovies: null,
 		trailerVideo: null,
 	},
 	reducers: {
 		addNowPlayingMovies: (state, action) => {
 			state.nowPlayingMovies = action.payload;
+		},
+		addPopularMovies: (state, action) => {
+			state.popularMovies = action.payload;
+		},
+		addTopRatedMovies: (state, action) => {
+			state.topRatedMovies = action.payload;
+		},
+		addUpcomingMovies: (state, action) => {
+			state.upComingMovies = action.payload;
 		},
 		addTrailerVideo: (state, action) => {
 			state.trailerVideo = action.payload;
@@ -17,6 +29,6 @@ const movieSlice = createSlice({
 })
 
 
-export const { addNowPlayingMovies, addTrailerVideo } = movieSlice.actions
+export const { addNowPlayingMovies, addPopularMovies, addTopRatedMovies, addUpcomingMovies, addTrailerVideo } = movieSlice.actions
 
 export default movieSlice.reducer; 
