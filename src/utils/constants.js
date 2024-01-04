792d5b695ccb9707098bdf4bd7d1314d83aa10c1
Haseeb-creator <mdhaseeb769@gmail.com>
2024-01-04 22:10:13 +0530

    added background image and language support

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -11,3 +11,13 @@ export const API_OPTIONS = {
 };
 
 export const IMG_URL = 'https://image.tmdb.org/t/p/w500/'
+
+export const BG_URL = "https://assets.nflxext.com/ffe/siteui/vlv3/b4c7f092-0488-48b7-854d-ca055a84fb4f/5b22968d-b94f-44ec-bea3-45dcf457f29e/IN-en-20231204-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+
+
+export const SUPPORTED_LANGUAGE = [
+	{ identifier: 'english', name: 'English' },
+	{ identifier: 'hindi', name: 'Hindi' },
+	{ identifier: 'kannada', name: 'Kannada' },
+	{ identifier: 'spanish', name: 'Spanish' },
+]
