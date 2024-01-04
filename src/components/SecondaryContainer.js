c0ac7f0ad7c46975daadc3dae4233d35ab1967b8
Haseeb-creator <mdhaseeb769@gmail.com>
2024-01-04 15:55:07 +0530

    added movieList component to show all movie

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import MovieList from './MovieList'
+import { useSelector } from 'react-redux'
 
 const SecondaryContainer = () => {
+
+	const movies = useSelector((store) => store.movies)
+
+
 	return (
-		<div>SecondaryContainer</div>
+
+		<div className='bg-black'>
+			<div className='-mt-44 pl-8 relative z-20'>
+				{movies?.nowPlayingMovies &&
+					<MovieList title={'Now Playing'} movies={movies?.nowPlayingMovies} />
+				}
+				{movies?.topRatedMovies &&
+					<MovieList title={'Top Rated'} movies={movies?.topRatedMovies} />
+				}
+				{movies?.popularMovies &&
+					<MovieList title={'Popular'} movies={movies?.popularMovies} />
+				}
+				{movies?.upComingMovies &&
+					<MovieList title={'Upcoming Movies'} movies={movies?.upComingMovies} />
+				}
+			</div>
+		</div>
 	)
 }
 
